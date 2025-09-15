2206efa888287510af11ed6a0912f319e6b9ab43
Rahul Salvi <rsalvi006@gmail.com>
2025-09-15 13:07:37 -0700

    better shell syntax in pre-deploy

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -5,8 +5,10 @@ if [ ! -e .env ]; then
     echo "Creating .env"
     touch .env
     chmod 600 .env
-    echo "TS_AUTHKEY=$(get-authkey -tags tag:webserver -ephemeral -preauth)" >>.env
-    echo "ENCRYPTION_KEY=$(openssl rand -hex 16)" >>.env
-    echo "AUTH_SECRET=$(openssl rand -base64 32)" >>.env
-    echo "POSTGRES_PASSWORD=$(openssl rand -hex 16)" >>.env
+    {
+        echo "TS_AUTHKEY=$(get-authkey -tags tag:webserver -ephemeral -preauth)"
+        echo "ENCRYPTION_KEY=$(openssl rand -hex 16)"
+        echo "AUTH_SECRET=$(openssl rand -base64 32)"
+        echo "POSTGRES_PASSWORD=$(openssl rand -hex 16)"
+    } >>.env
 fi
